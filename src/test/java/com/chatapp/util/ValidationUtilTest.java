785404555cce6929169d98fddeb90000fb5a320f
Pankaj Kumar <83492104+hack2ai@users.noreply.github.com>
2026-09-03 11:44:22 +0530

    test: cover centralized input validation

---
+++ src/test/java/com/chatapp/util/ValidationUtilTest.java
@@ -0,0 +1,42 @@
+package com.chatapp.util;
+
+import com.chatapp.exception.ValidationException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ValidationUtilTest {
+
+    @Test
+    void acceptsValidUsernameAndEmail() {
+        assertDoesNotThrow(() -> ValidationUtil.validateUsername("chat_user_123"));
+        assertDoesNotThrow(() -> ValidationUtil.validateEmail("user@example.com"));
+    }
+
+    @Test
+    void rejectsUnicodeLookalikeUsername() {
+        assertThrows(ValidationException.class, () -> ValidationUtil.validateUsername("usеr123"));
+    }
+
+    @Test
+    void rejectsPasswordWithoutLetterAndDigit() {
+        assertThrows(ValidationException.class, () -> ValidationUtil.validatePassword("12345678"));
+        assertThrows(ValidationException.class, () -> ValidationUtil.validatePassword("abcdefgh"));
+    }
+
+    @Test
+    void acceptsPasswordAtBoundaryButRejectsOversizedInput() {
+        assertDoesNotThrow(() -> ValidationUtil.validatePassword("a1" + "x".repeat(6)));
+        assertDoesNotThrow(() -> ValidationUtil.validatePassword("a1" + "x".repeat(126)));
+        assertThrows(ValidationException.class, () -> ValidationUtil.validatePassword("a1" + "x".repeat(127)));
+    }
+
+    @Test
+    void validatesMessageAndGroupNameBoundaries() {
+        assertDoesNotThrow(() -> ValidationUtil.validateMessageContent("a".repeat(5000)));
+        assertThrows(ValidationException.class, () -> ValidationUtil.validateMessageContent("a".repeat(5001)));
+        assertDoesNotThrow(() -> ValidationUtil.validateGroupName("g".repeat(50)));
+        assertThrows(ValidationException.class, () -> ValidationUtil.validateGroupName("g".repeat(51)));
+    }
+}
