b0795e1d1cef6a8810d0018137af3cf3f83a5d8d
Pankaj Kumar <83492104+hack2ai@users.noreply.github.com>
2026-09-16 09:25:55 +0530

    security: scope MySQL grants to known tables

--- docker/mysql/02-application-grants.sql
+++ docker/mysql/02-application-grants.sql
@@ -2,4 +2,9 @@
 -- Schema changes and privilege management remain the responsibility of the
 -- administrative/root account used to initialize or migrate the database.
 REVOKE ALL PRIVILEGES, GRANT OPTION ON `chatapp_db`.* FROM 'chatapp_user'@'%';
-GRANT SELECT, INSERT, UPDATE, DELETE ON `chatapp_db`.* TO 'chatapp_user'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `chatapp_db`.`users` TO 'chatapp_user'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `chatapp_db`.`private_messages` TO 'chatapp_user'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `chatapp_db`.`private_attachments` TO 'chatapp_user'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `chatapp_db`.`chat_groups` TO 'chatapp_user'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `chatapp_db`.`group_members` TO 'chatapp_user'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `chatapp_db`.`group_messages` TO 'chatapp_user'@'%';
