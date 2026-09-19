263cca720a0684a91d03356a1513d219880ce6da
Pankaj Kumar <83492104+hack2ai@users.noreply.github.com>
2026-09-19 18:47:54 +0530

    fix: correct attachment validator test escaping

--- src/test/java/com/chatapp/service/AttachmentValidatorTest.java
+++ src/test/java/com/chatapp/service/AttachmentValidatorTest.java
@@ -39,7 +39,7 @@ class AttachmentValidatorTest {
 
     @Test
     void acceptsValidPdfSignature() throws Exception {
-        AttachmentValidator.validateContent("application/pdf", "%PDF-1.7\\n".getBytes(StandardCharsets.US_ASCII));
+        AttachmentValidator.validateContent("application/pdf", "%PDF-1.7\n".getBytes(StandardCharsets.US_ASCII));
     }
 
     @Test
@@ -73,7 +73,7 @@ class AttachmentValidatorTest {
 
     @Test
     void acceptsValidJson() throws Exception {
-        AttachmentValidator.validateContent("application/json", "{\\"message\\":\\"hello\\",\\"ok\\":true}".getBytes(StandardCharsets.UTF_8));
+        AttachmentValidator.validateContent("application/json", "{\"message\":\"hello\",\"ok\":true}".getBytes(StandardCharsets.UTF_8));
     }
 
     @Test
